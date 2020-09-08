99697179b5edc3dc11b5b2d5a393e153b73ac90c
Nolan <nolan.boxill@neofinancial.com>
2020-09-08 15:20:02 -0600

    operator completed with simple switch case

--- classes/calc.class.php
+++ classes/calc.class.php
@@ -1,5 +1,38 @@
 <?php 
     class Calc {
+        public $operator;
+        public $num1;
+        public $num2;
 
+    public function __constructor(string $operator, int $one, int $two, int $three) {
+        $this->operator = $operator;
+        $this->num1= $two;
+        $this->num2 = $three;
+    }
+
+    public function calculator(int $operator) {
+        switch ($operator) {
+            case 'add';
+            $result = $this->num1 + $this->num2;
+            return $result;
+
+            case 'sub';
+            $result = $this->num1 - $this->num2;
+            return $result;
+
+            case 'div';
+            $result = $this->num1 / $this->num2;
+            return $result;
+
+            case 'mul';
+            $result = $this->num1 * $this->num2;
+            return $result;
+
+            break;
+            default: 
+                echo "error";
+        break;
+        }
+    }
 }
 ?>
